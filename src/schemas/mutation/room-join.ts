3f3a2cd81dd5c57016e27e08ccc50d4baac1fd79
chiragmerchant <42070813+chiragmerchant@users.noreply.github.com>
2024-09-13 14:46:23 -0700

    Makefile and package.json run script entries, formatting, license etc
    Makefile and npm now suitable for windows development, checks and tests
    Added license text to license and header
    Updated licenses in all code, ran format
    
    Addresses requests from PR 6
    Still pending 5 warnings.

--- src/schemas/mutation/room-join.ts
+++ src/schemas/mutation/room-join.ts
@@ -1,8 +1,11 @@
 /*
+This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved.
+Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting: USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
 
+The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 
-import { GraphQLID, GraphQLString } from "graphql";
+import { GraphQLID, GraphQLString, GraphQLObjectType } from "graphql";
 import RoomModel, { Room, RoomType } from "../models/Room";
 import PlayerModel from "../models/Player";
 
@@ -13,7 +16,7 @@ export const joinRoom = {
     roomId: { type: GraphQLID },
   },
   resolve: async (
-    _root: any,
+    _root: GraphQLObjectType,
     args: {
       playerId: string;
       roomId: string;
--- src/schemas/query/fetch-rooms.ts
+++ src/schemas/query/fetch-rooms.ts
@@ -1,8 +1,16 @@
 /*
+This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved.
+Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting: USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
 
+The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 
-import { GraphQLList, GraphQLString, GraphQLBoolean } from "graphql";
+import {
+  GraphQLList,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLObjectType,
+} from "graphql";
 import RoomModel, { Room, RoomType } from "../models/Room";
 
 export const fetchRooms = {
@@ -12,7 +20,7 @@ export const fetchRooms = {
     deletedRoom: { type: GraphQLBoolean },
   },
   resolve: async (
-    _root: any,
+    _root: GraphQLObjectType,
     args: { game: string; deletedRoom: boolean }
   ): Promise<Room[]> => {
     if (args.game) {
